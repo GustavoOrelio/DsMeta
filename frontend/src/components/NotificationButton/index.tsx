e0221a9d9c75cf1bfc62620733efc4a6b62c49f4
Gustavo Orelio <oreliogustavo@gmail.com>
2022-07-15 23:00:54 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,13 +1,22 @@
-import icon from '../../assets/img/notification-icon.svg';
-import './styles.css';
+import axios from "axios";
+import icon from "../../assets/img/notification-icon.svg";
+import { BASE_URL } from "../../utils/request";
+import "./styles.css";
 
-function NotificationButton() {
-    return(
-        <div className="dsmeta-red-btn">
-        <img src={icon} alt="Notificar" />
-      </div>
-    );
-  }
-  
-  export default NotificationButton
-  
+type Props = {
+  saleId: number;
+};
+
+function handleClick(id : number){
+  axios(`${BASE_URL}/sales/${id}/notification`).then(response =>{})
+}
+
+function NotificationButton({ saleId }: Props) {
+  return (
+    <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
+      <img src={icon} alt="Notificar" />
+    </div>
+  );
+}
+
+export default NotificationButton;
--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -17,13 +17,14 @@ function SalesCard() {
   const [sales, setSales] = useState<Sale[]>([]);
 
   useEffect(() => {
-
     const dmin = minDate.toISOString().slice(0, 10);
     const dmax = maxDate.toISOString().slice(0, 10);
 
-    axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`).then((response) => {
-      setSales(response.data.content);
-    });
+    axios
+      .get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
+      .then((response) => {
+        setSales(response.data.content);
+      });
   }, [minDate, maxDate]);
 
   return (
@@ -66,14 +67,16 @@ function SalesCard() {
               return (
                 <tr key={sale.id}>
                   <td className="show992">{sale.id}</td>
-                  <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>
+                  <td className="show576">
+                    {new Date(sale.date).toLocaleDateString()}
+                  </td>
                   <td>{sale.sellerName}</td>
                   <td className="show992">{sale.visited}</td>
                   <td className="show992">{sale.deals}</td>
                   <td>R$ {sale.amount.toFixed(2)}</td>
                   <td>
                     <div className="dsmeta-red-btn-container">
-                      <NotificationButton />
+                      <NotificationButton saleId={sale.id} />
                     </div>
                   </td>
                 </tr>
